365138906108f5e0e463985fc47d4f81e50f3db8
macdo <macdo@DESKTOP-PF4F3N3>
2023-03-08 19:39:32 -0700

    first commit 2

---
+++ src/checkthemonth/CheckTheMonth.java
@@ -0,0 +1,18 @@
+package checkthemonth;
+
+import java.util.Scanner;
+
+public class CheckTheMonth {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("This will tell us what month it is hopefully");
+        
+        String[] january, february, march, april, may, june, july, august, september, october, november, december;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please pick a month from the displayed options.");
+        
+    }
+    
+}
